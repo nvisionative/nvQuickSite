c4d3e02374305509cabda03f183daebbc9de8a61
David Poindexter <dpoindexter@nvisionative.com>
2016-03-23 13:36:42 -0400

    Update project references

--- Settings.cs
+++ Settings.cs
@@ -1,5 +1,21 @@
-﻿namespace nvQuickSite.Properties {
-    
+﻿//Copyright (c) 2016 nvisionative, Inc.
+
+//This file is part of nvQuickSite.
+
+//nvQuickSite is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//nvQuickSite is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
+
+namespace nvQuickSite.Properties {
     
     // This class allows you to handle specific events on the settings class:
     //  The SettingChanging event is raised before a setting's value is changed.
