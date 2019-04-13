0485383d8d2f9bdbdefc8641048fda86e8c05b0c
David Poindexter <dpoindexter@nvisionative.com>
2019-04-12 23:12:31 -0400

    Add user settings screen and support for Release Candidates

---
+++ nvQuickSite/UserSettings.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace nvQuickSite
+{
+    partial class UserSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new MetroFramework.Controls.MetroLabel();
+            this.btnSave = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.chkShowReleaseCandidates = new MetroFramework.Controls.MetroCheckBox();
+            this.dialogIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dialogIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblMessage.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblMessage.Location = new System.Drawing.Point(111, 18);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(86, 25);
+            this.lblMessage.Style = MetroFramework.MetroColorStyle.Blue;
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Message";
+            this.lblMessage.UseStyleColors = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSave.Location = new System.Drawing.Point(330, 129);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.metroButton2.Location = new System.Drawing.Point(429, 129);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "Cancel";
+            // 
+            // chkShowReleaseCandidates
+            // 
+            this.chkShowReleaseCandidates.AutoSize = true;
+            this.chkShowReleaseCandidates.Location = new System.Drawing.Point(118, 53);
+            this.chkShowReleaseCandidates.Name = "chkShowReleaseCandidates";
+            this.chkShowReleaseCandidates.Size = new System.Drawing.Size(156, 15);
+            this.chkShowReleaseCandidates.TabIndex = 3;
+            this.chkShowReleaseCandidates.Text = "Show Release Candidates";
+            this.chkShowReleaseCandidates.UseVisualStyleBackColor = true;
+            // 
+            // dialogIcon
+            // 
+            this.dialogIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.dialogIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.dialogIcon.Image = global::nvQuickSite.Properties.Resources.user_settings;
+            this.dialogIcon.Location = new System.Drawing.Point(11, 23);
+            this.dialogIcon.Name = "dialogIcon";
+            this.dialogIcon.Size = new System.Drawing.Size(91, 88);
+            this.dialogIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.dialogIcon.TabIndex = 4;
+            this.dialogIcon.TabStop = false;
+            // 
+            // UserSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(527, 170);
+            this.Controls.Add(this.dialogIcon);
+            this.Controls.Add(this.chkShowReleaseCandidates);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "UserSettings";
+            ((System.ComponentModel.ISupportInitialize)(this.dialogIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel lblMessage;
+        private MetroFramework.Controls.MetroButton btnSave;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroCheckBox chkShowReleaseCandidates;
+        private System.Windows.Forms.PictureBox dialogIcon;
+    }
+}
